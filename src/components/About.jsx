b2f1c6b474716f19272c67978567d4333dd25bf9
clausaji <c.sanchezjimenez@qquest.nl>
2023-05-08 16:39:54 +0200

    Wrong data in json

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,13 +6,33 @@ export default function About() {
   return (
     <div className="aboutPage">
       <img src={myself} />
-      <div>
+      <div className="pDiv">
         <p>
-          I get energy from solving puzzles and have developed a passion for
-          coding. By thinking creatively, persevering, and learning quickly, I
-          am able to tackle challenging projects and successfully complete them.
-          I am also a team player and I love working with colleagues to come up
-          with and implement the best solutions. That gives me energy.
+          Hello, I am Claudia. I come from Spain but I have lived the last 7
+          years in The Netherlands. I have a great passion for experimental
+          films and books, because they make you look at things differently. I
+          divide my other hobbies between summer and winter. Perhaps that is
+          because of my Spanish background. For example, in the summer I like to
+          go swimming outside and in the winter I like to spend my free time
+          playing complicated board games with friends, such as Gloomhaven or
+          Pandemic. We then have evenings of fun together.
+        </p>
+        <p>
+          My first contact with IT was with Python. During high school, Choose
+          Your Own Adventure games were trendy and I made my own game with
+          Ren'Py, a graphic novel development tool based on Python. Since then I
+          have made versions of many classic games using JS and Java. In my work
+          as a film distributor, I also maintained websites and created an
+          accounting system for freelancers to do the administration as
+          efficiently as possible.
+        </p>
+        <p>
+          I think programming is the most fun aspect of IT, because of the
+          search for solutions that work. Moreover, it requires a lot of
+          analytical skills, making connections and puzzling. That fits me. I
+          also like to work on projects with an impact. I enjoy it when a
+          product really makes an impact and that it is written about in the
+          media. That makes me proud!
         </p>
 
         <span>clausaji31@gmail.com</span>
--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -3,7 +3,7 @@ const projects = [
     title: "Netflix Clone",
     photo: "netflix-clone.png",
     description:
-      " Last Christmas I worked on a clone of Netflix. It was my first full-stack multi-language application. I made a front end in React, Javascript, HTML and CSS and I added an api in Java with Spring Boot. The api connects the front end with a database in PostgreSQL. The application also consumes the TMDB api, which gives access to a chuge catalogue of films. I use this catalogue to display the films in the front end and store in my own database your film preferences (liked films, loved films, your watchlist...). The page also includes a search function and a basic algorithm that places users' favorite genres on top of the website. \n This project helped me better understand the structures of apis and allowed me to really work in my CSS skills. It was nice to have a design that I could copy. It was a challenge to learn new structures and effects. ",
+      " Last Christmas I worked on a clone of Netflix. It was my first full-stack multi-language application. I made a front end in React, Javascript, HTML and CSS and I added an api in Java with Spring Boot. The api connects the front end with a database in PostgreSQL. The application also consumes the TMDB api, which gives access to a huge catalogue of films. I use this catalogue to display the films in the front end and store in my own database your film preferences (liked films, loved films, your watchlist...). The page also includes a search function and a basic algorithm that places users' favorite genres on top of the website. \n This project helped me better understand the structures of apis and allowed me to really work in my CSS skills. It was nice to have a design that I could copy. It was a challenge to learn new structures and effects. ",
     link: "https://netflixclone-57465.web.app/",
     githubRepo: [
       "https://github.com/klaw94/netflixapi",
@@ -24,7 +24,8 @@ const projects = [
     title: "IT Landscape App",
     photo: "itlandschap.png",
     description:
-      "This is an application developed by Qquest with the goal of mapping the company's IT landscape in an easy way. The application was developed using React and Java Spring Boot and with a PostgreSQL database. My tasks consist of adding functionalities to both the front-end and the back-end. Examples of this include a secure authorization system for the back-end or basic functionality in the front-end, such as adding tasks to different apps or an administrator panel where applications can be added or updated. By contributing to this application, I have been able to achieve a steep learning curve about the structure of APIs and web applications.",
+      "This is an application developed by Qquest with the goal of mapping the company's IT landscape. The application was developed using React, Java Spring Boot and with a PostgreSQL database. My tasks consisted of adding functionalities to both the front-end and the back-end. Examples of this include a secure authorization system for the back-end or developing different panels to associate tasks to different apps or updating the apps'information." +
+      " I am especially proud of the api, for which I was responsible. It was a more complex api than I was used to, because it included a big database, with very different relationships between tables. I learned a lot about data models and structures and how can you better organise your tables, in order to get data more efficiently.",
     link: "",
     githubRepo: ["https://gitlab.com/Reinaldinus/it-plaat-app"],
     languages: [
@@ -42,7 +43,7 @@ const projects = [
     title: "Pandemic",
     photo: "pandemic.png",
     description:
-      "The project where I am working on currently. It is an online version of the game Pandemic, where 4 players have to move through a world, collecting cubes. I am making it with React and Javascript.",
+      "I am currtently working on this virtual version of the boardgame Pandemic. In this game 4 players have to move through a world map, cleaning cubes. I am making it with React and Javascript. I really like boardgames, so it is a very entertaining project, and it also helps me understand React better. The game has many rules and a strict playing order. Because of that, I have to pay extra attention to React's rerendering patterns.",
     link: "",
     githubRepo: ["https://github.com/klaw94/pandemic"],
     languages: ["Javascript", "React"],
@@ -70,7 +71,7 @@ const projects = [
     photo: "frog.png",
     description:
       "Game made in Vanilla Javascript. A frog has to cross the road avoiding the cars and cross the river jumping from log to log.",
-    link: "https://blackjackjs-799a0.web.app/",
+    link: "https://frog-6daaa.web.app/index.html",
     githubRepo: ["https://github.com/klaw94/froggame"],
     languages: ["Javascript", "Firebase"],
   },
